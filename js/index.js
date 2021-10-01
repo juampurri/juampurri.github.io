70b1ffd0a1236464ee4eea97863f6c02948309da
Juan Bravo <bravo.juan@live.com>
2021-10-01 20:00:35 -0300

    menu hamburguesa funcional v4 igual todavia no esta terminado

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-const menu = document.querySelector(".nav__menu");
-const openMenuBtn = document.querySelector(".open-menu");
-const closeMenuBtn = document.querySelector(".close-menu");
-function togglemenu() {
-  menu.classList.toggle("menu_opened");
-}
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+});
